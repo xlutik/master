681a69ec676717fdaa67fd9bf080362ded50c57b
ls\lutik <sergey.lutiy@lime-systems.com>
2023-01-20 17:13:16 +0200

    Check data report NBU

---
+++ xFileTo#File/F01XMLTOTEXT/F02xmlToText/F02xmlToText/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Data;
+
+namespace F02xmlToText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Transform File #02 from XML To Text");
+            if (args.Count<string>() == 0 || args.Count<string>() == 1)
+            {
+                Console.WriteLine("F02xmlToText <InputFileName> <OutputFileName> [<ADDUNION>]");
+                return;
+            }
+
+
+            string FileInput = null;
+            string FileOutput = null;
+            bool addUnionAll = false;
+            if (args.Count<string>() > 0 && (args[0] != "")) FileInput = args[0];
+            if (args.Count<string>() > 1 && (args[1] != "")) FileOutput = args[1];
+            if (args.Count<string>() > 2)
+            {
+                string S = args[2]; S.ToUpper();
+                if (S == "ADDUNION")
+                    addUnionAll = true;
+            }
+
+            Console.WriteLine(string.Format(" Input File : {0}", FileInput));
+            Console.WriteLine(string.Format("Output File : {0}", FileOutput));
+
+            //Console.ReadLine();
+            StreamWriter wo = Transform.NewFileName(FileOutput);
+            DataSet ds = Transform.LoadFileData(FileInput);
+            Transform.WriteHeader(wo, ds);
+            Transform.TransformData(wo, ds, addUnionAll);
+            Transform.CloseFile(wo);
+        }
+    }
+}
---
+++ xFileTo#File/F01XMLTOTEXT/F02xmlToText/F02xmlToText/Transform.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Data;
+
+namespace F02xmlToText
+{
+    public static class Transform
+    {
+        public static StreamWriter NewFileName(string FileName = null)
+        {
+            if (FileName == null)
+            {
+                FileName = Path.GetRandomFileName();
+                Console.WriteLine(string.Format("New FileName = {0}", FileName));
+            }
+            return new StreamWriter(FileName, false);
+        }
+        public static DataSet LoadFileData(string FileName)
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml(FileName);
+            return ds;
+        }
+        public static void WriteHeader(StreamWriter fstream, DataSet ds)
+        {
+            fstream.WriteLine("");
+            DataRow row = ds.Tables["HEAD"].Rows[0];
+            fstream.WriteLine(string.Format("00={0}=________=________={0}=____={1}=__={2}", row["REPORTDATE"].ToString().Replace(".", ""), row["EDRPOU"].ToString(), ds.Tables["DATA"].Rows.Count ));
+            fstream.WriteLine("#02=");
+        }
+        public static void TransformData(StreamWriter fstream, DataSet ds, bool addUnionAll = false)
+        {
+            Console.WriteLine(string.Format("В файле     : {0} записей с данными.", ds.Tables["DATA"].Rows.Count));
+            
+            foreach (DataRow row in ds.Tables["DATA"].Rows)
+            {
+                string AP1 = row["T020"].ToString().Trim();
+                string NE = (row["R030"].ToString() == "980") ?"0":"1";
+                string BBBB = row["R020"].ToString();
+                string VVV = row["R030"].ToString();
+                string SumNom = row["T071"].ToString().Replace(".0", "").Trim();
+                string SumEkv = row["T070"].ToString().Trim();
+
+                string RetValue = string.Empty;
+                if (NE == "1")
+                {
+                    if (SumNom != "0")
+                    {
+                        RetValue = string.Format("{0}{1}{2}{3}1={4}", AP1, NE, BBBB, VVV, SumNom);
+                        if (addUnionAll)
+                            RetValue = string.Format("SELECT '{0}'           UNION ALL", RetValue);
+                        fstream.WriteLine(RetValue);
+                    }
+                    if (SumEkv != "0")
+                    {
+                        RetValue = string.Format("{0}{1}{2}{3}1={4}", AP1, "0", BBBB, VVV, SumEkv);
+                        if (addUnionAll)
+                            RetValue = string.Format("SELECT '{0}'           UNION ALL", RetValue);
+                        fstream.WriteLine(RetValue);
+                    }
+                }
+                else
+                {
+                    if (SumEkv != "0")
+                    {
+                        RetValue = string.Format("{0}{1}{2}{3}1={4}", AP1, "0", BBBB, VVV, SumEkv);
+                        if (addUnionAll)
+                            RetValue = string.Format("SELECT '{0}'           UNION ALL", RetValue);
+                        fstream.WriteLine(RetValue);
+                    }
+                }
+            }
+
+        }
+        public static void CloseFile(StreamWriter writer)
+        {
+            writer.Close();
+        }
+
+
+    }
+}
